57ec921e7bf7f25c954214e17e2b6e36b14e9bc6
Vandan Kambodi <vandankambodi27@gmail.com>
2025-05-27 10:09:41 +0530

    Flutter Files
    Add all necessary files for flutter

---
+++ android/app/src/main/kotlin/com/example/crednix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.crednix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:crednix/SplashPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock orientation to portrait only
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crednix',
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Welcome to Crednix")),
+      body: Center(child: Text("Dashboard")),
+    );
+  }
+}
